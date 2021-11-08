a27cd35c24d61a0472b38ed97d80a1afe804c21a
Mariusz Walania <mariusz.walania@gmail.com>
2021-11-08 13:00:24 +0100

    bin scripts refactoring

--- truffle-config.js
+++ truffle-config.js
@@ -9,7 +9,9 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/public/contracts"),
   networks: {
     develop: {
-      port: 8545
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
     },
     ropsten: {
       provider: function() {
